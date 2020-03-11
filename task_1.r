69a3ca5a36c2f9206da6dd15c4c1b9654fc8953d
Vishesh Tripathi <33310569+12VISHESH@users.noreply.github.com>
2020-03-12 00:55:55 +0530

    readable format

--- task_1.r
+++ task_1.r
@@ -2,19 +2,34 @@ dir.create("data_raw", showWarnings = FALSE)
 
 download.file(url = "https://ndownloader.figshare.com/files/2292171",
               destfile = "data_raw/portal_mammals.sqlite", mode = "wb") ##loading the file 
+
 library(dplyr) ##importing the library for connecting database 
+
 library(dbplyr) ##importing the library for connecting database
+
 mammals <- DBI::dbConnect(RSQLite::SQLite(), "data_raw/portal_mammals.sqlite") ##connecting database 
+
 src_dbi(mammals) ## checking the connectivity
+
 tbl(mammals, sql("SELECT year, species_id, plot_id FROM surveys")) ##Querying the database with the SQL syntax
+
 surveys <- tbl(mammals, "surveys")
+
 surveys %>%
-  select(year, species_id, plot_id)
+
+select(year, species_id, plot_id)
+
+
 head(surveys, n = 10)
+
 nrow(surveys)
+
 data_subset <- surveys %>%
-  filter(weight < 5) %>%
-  select(species_id, sex, weight)
+
+filter(weight < 5) %>%
+
+select(species_id, sex, weight)
+
 
 data_subset %>%
   select(-sex)
