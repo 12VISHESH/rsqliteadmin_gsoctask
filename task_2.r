00cd7d6a7bd5063886300c0574a27dc7e7074280
Vishesh Tripathi <33310569+12VISHESH@users.noreply.github.com>
2020-03-12 19:26:30 +0530

    task update

--- task_2.r
+++ task_2.r
@@ -1,19 +1,44 @@
-library(DBI)
-# Create an ephemeral in-memory RSQLite database
-con <- dbConnect(RSQLite::SQLite(), dbname = ":memory:")
-dbListTables(con)
-dbWriteTable(con, "mtcars", mtcars)
-dbListTables(con)
-dbListFields(con, "mtcars")
-dbReadTable(con, "mtcars")
-res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
-dbFetch(res)
-dbClearResult(res)
-res <- dbSendQuery(con, "SELECT * FROM mtcars WHERE cyl = 4")
-while(!dbHasCompleted(res)){
-  chunk <- dbFetch(res, n = 5)
-  print(nrow(chunk))
-}
-dbClearResult(res)
+library(sqldf)
 
-dbDisconnect(con)
+employess <- read.csv()
+
+View(employess)
+
+male_employess<- function(sqldf){
+  sqldf("SELECT * FROM employess where gender ='m'")
+}
+
+name_count_employee <- function(sqldf){
+  sqldf("SELECT firstname,count(firstname) as occurancees
+        FROM employes
+        where firstname!='rudi'
+        Group by firstname")
+}
+
+# using 'lower'to evaluate all names  as lowercase to ensure case insensitivity
+employess_call <- function(sqldf){
+  sqlfd("SELECT*
+        CASE
+        WHEN lower(firstname)='stewart'THEN 1
+        WHEN lower(firstname)='stewart'THEN 1
+        WHEN lower(firstname)='stewart'THEN 1
+        WHEN lower(firstname)='stewart'THEN 1
+        ELSE 0
+        FRom employess")
+}
+
+#multi- table operation#
+
+left_join <- function(sqldf){
+  sqldf("SELECT *
+        from order b
+        LEFt join employee a ona.id=b.id")
+} 
+
+# "right join" isn't supported in sqldf but switiching order of tables and left join is equivalent function
+
+right_join <- function(sqldf){
+  sqldf("SELECT *
+        FROM order b
+        LEFT JOIN employee a ON a.id=b.id")
+}
